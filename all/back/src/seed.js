1a17ee14e5fdae0e386a03278c9ca02be0faedc1
fede-dev <fedenriquez@hotmail.com>
2021-09-15 17:27:41 -0300

    seed-obj

---
+++ all/back/src/seed.js
@@ -0,0 +1,12 @@
+const arr = [];
+const obj = {
+  pic: "https://clipart-best.com/img/face/face-clip-art-23.png",
+  name: "Barack Obama",
+  description:
+    "The 51th president of the United State of America, father of 2 and married with Sarah Country.",
+};
+arr.push(obj);
+//console.log(arr);
+for (let i = 0; i < arr.length; i++) {
+  console.log(arr[i]);
+}
